831be0f5037e2b6cef63aca706f3a128ab2e8de4
Avram Traian <traian.avram1@gmail.com>
2024-12-20 15:27:35 +0200

    Core: Implemented the core memory manipulation operations

---
+++ core/base/memory/memory_operations.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#include <core/base/types.h>
+
+namespace arc {
+
+void copy_memory(void* destination_buffer, const void* source_buffer, usize byte_count);
+void set_memory(void* destination_buffer, u8 byte_value, usize byte_count);
+void zero_memory(void* destination_buffer, usize byte_count);
+
+}
