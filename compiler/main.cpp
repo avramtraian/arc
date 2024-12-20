a0e408d20a1f774d169971241662f5c750c3d086
Avram Traian <traian.avram1@gmail.com>
2024-12-20 15:33:12 +0200

    Everywhere: Implemented the executables entry points

---
+++ compiler/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#include <core/base/argument_parser.h>
+
+namespace arc {
+
+static void compiler_main(const CommandLineArguments& arguments)
+{}
+
+}
+
+int main(int argc, char** argv)
+{
+    arc::CommandLineArguments arguments = {};
+    arguments.arguments = argv;
+    arguments.argument_count = static_cast<arc::u32>(argc);
+    arc::compiler_main(arguments);
+    return 0;
+}
