b15c137391eca234a6d7ed94dd00a3da26f2fa09
Avram Traian <traian.avram1@gmail.com>
2024-12-24 23:57:18 +0200

    Bytecode: Delay IP increment in `Disassembler::instructions_as_string`

--- bytecode/disassembler.cpp
+++ bytecode/disassembler.cpp
@@ -18,8 +18,9 @@ String Disassembler::instructions_as_string() const
     StringBuilder builder;
     usize instruction_pointer = 0;
     while (m_package.instruction_pointer_is_valid(instruction_pointer)) {
-        const auto& instruction = m_package.fetch_instruction(instruction_pointer++);
+        const auto& instruction = m_package.fetch_instruction(instruction_pointer);
         builder.append("[{}] {}\n"sv, instruction_pointer, instruction.to_string());
+        ++instruction_pointer;
     }
 
     return builder.release_string();
