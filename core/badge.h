1f4098edf98631d435ae7b633f621e68d1117721
Avram Traian <traian.avram1@gmail.com>
2024-12-27 11:59:15 +0200

    Core: Implemented the `Badge` utility class

---
+++ core/badge.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#include <core/types.h>
+
+namespace arc::core {
+
+template<typename T>
+class Badge {
+    ARC_MAKE_NONCOPYABLE(Badge);
+    ARC_MAKE_NONMOVABLE(Badge);
+
+    // NOTE: Because the only available constructor is marked as private, the only typename that
+    //       can instantiate this badge is 'T'.
+    friend T;
+
+public:
+    ~Badge() = default;
+
+private:
+    Badge() = default;
+};
+
+}
+
+namespace arc {
+using core::Badge;
+}
